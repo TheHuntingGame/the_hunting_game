9e3920ac46d042f0e57eb40858f376e82b174a46
baum456 <mingqi_li@icloud.com>
2024-05-20 20:53:42 +0200

    switch bewteen screens main,shop,chat,settings
    NOTE: Setup screen before that reqiured: Create Game and Join Game with Logo and lobby screen before gameplay starts

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:the_hunting_game/pages/chat_page.dart';
+import 'package:the_hunting_game/pages/settings_page.dart';
+import 'package:the_hunting_game/pages/shop_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  //Things for switch screens_BEGIN
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    //main
+    MainPage(),
+    //shop
+    ShopPage(),
+    //chat
+    ChatPage(),
+    //settings
+    SettingsPage(),
+  ];
+  //Things for switch screens_END
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TheHuntingGame'),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Material(
+  elevation: 5.0,
+  child: BottomNavigationBar(
+    currentIndex: _selectedIndex,
+    onTap: _navigateBottomBar,
+    unselectedItemColor: Colors.grey, // Color for unselected items
+    selectedItemColor: Colors.blue, // Color for selected item
+    showSelectedLabels: true, // Always show labels for selected items
+    showUnselectedLabels: true, // Always show labels for unselected items
+    type: BottomNavigationBarType.fixed,
+    items: const [
+      //main
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+      //shop
+      BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Shop'),
+      //chat
+      BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+      //settings
+      BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+    ],
+  ),
+),
+    );
+  }
+}
