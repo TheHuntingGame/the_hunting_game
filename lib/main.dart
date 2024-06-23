4374b759623a302f1b2bd52fa9f112fb78b95b0b
baum456 <mingqi_li@icloud.com>
2024-06-23 15:28:53 +0200

    smooth transition between dark and light mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:the_hunting_game/authentication/login_page.dart';
 import 'package:the_hunting_game/authentication/splashscreen.dart';
 import 'package:the_hunting_game/screens/intro_screen.dart';
-import 'package:the_hunting_game/screens/main_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -16,8 +14,7 @@ Future<bool> isFirstTimeOpen() async {
   return isFirstTime;
 }
 
-
-Future <void> main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await Supabase.initialize(
@@ -28,32 +25,75 @@ Future <void> main() async {
   runApp(MyApp(firstTime));
 }
 
-  //initialize supabase client
-  final supabase = Supabase.instance.client;
+final supabase = Supabase.instance.client;
 
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final bool firstTime;
 
   const MyApp(this.firstTime, {super.key});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        colorSchemeSeed: const Color.fromARGB(255, 0, 200, 255),
-        useMaterial3: true, // Enable Material 3
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xFF05053A),
-      ),
-      home: firstTime ? IntroScreen() : AuthPage(),
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, _) {
+        final brightness = MediaQuery.of(context).platformBrightness;
+        final isDarkMode = brightness == Brightness.dark;
+
+        if (isDarkMode) {
+          _animationController.forward();
+        } else {
+          _animationController.reverse();
+        }
+
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.light,
+            colorSchemeSeed: const Color.fromARGB(255, 0, 200, 255),
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            colorSchemeSeed: const Color(0xFF05053A),
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.system,
+          home: widget.firstTime ? IntroScreen() : AuthPage(),
+          builder: (context, child) {
+            return AnimatedTheme(
+              data: ThemeData(brightness: isDarkMode ? Brightness.dark : Brightness.light),
+              duration: const Duration(milliseconds: 500),
+              child: child!,
+            );
+          },
+        );
+      },
     );
   }
 }
+
 
